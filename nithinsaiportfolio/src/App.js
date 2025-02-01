cb5969bc4acedef732a126fe81cdf463759ca9c5
Nithin Sai Mikkilineni <nithinsaimikkilineni@Nithins-MacBook-Air.local>
2025-02-01 16:33:38 -0600

    started project portfolio

---
+++ nithinsaiportfolio/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>
+            Welcome to Nithin's Portfolio.
+          </h1>
+          <img src="https://pics.clipartpng.com/Under_Construction_Warning_Sign_PNG_Clipart-839.png" height="300" >
+          </img>
+      </header>
+    </div>
+  );
+}
+
+export default App;

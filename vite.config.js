921d6e299f5da9e8d2d27931e03162b142b064e9
Nithin Sai <mikkilineninithinsai9@gmail.com>
2025-10-09 12:09:42 -0500

    smaller changes

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,10 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   base: "/",
 })
